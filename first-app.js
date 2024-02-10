c0d43817a9997369d37aafa6aeab12a3c78938af
AVINASH KUMAR <Aviproo>
2024-02-10 10:24:13 +0530

    Request / Response

--- first-app.js
+++ first-app.js
@@ -1,5 +1,32 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if (url == "/home") {
+    res.write("<html>");
+    res.write("<head><title>My first page</title></head>");
+    res.write("<body><h1>hello this is home page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url == "/about") {
+    res.write("<html>");
+    res.write("<head><title>My first page</title></head>");
+    res.write("<body><h1>hello this is about page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url == "/node") {
+    res.write("<html>");
+    res.write("<head><title>My first page</title></head>");
+    res.write("<body><h1>hello this is nodejs project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>hello from my Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
-server.listen(200);
+server.listen(3000);
