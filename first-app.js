1b8c2a5e9c1310addcd6af8711894638e07a562e
AVINASH KUMAR <Aviproo>
2024-02-15 12:23:04 +0530

    Intro to Middlewares

--- first-app.js
+++ first-app.js
@@ -1,7 +1,23 @@
 const http = require("http");
+const bodyparser = require("body-parser");
 const express = require("express");
+
 const app = express();
-app.use((req, res, next) => {
-  res.send("hello");
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="POST">
+      <input type="text" name="title"/>
+      <button type="submit">Add Product</button>
+    </form>`
+  );
+});
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+app.use("/", (req, res, next) => {
+  res.send("hello app");
 });
 app.listen(3000);
